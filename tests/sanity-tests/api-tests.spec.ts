c13170e8b64e3cdaff6134d21a8cb318e5113b47
spaddz <77231276+spaddz@users.noreply.github.com>
2025-01-21 23:24:13 +0000

    add api tests and bug report

--- tests/sanity-tests/api-tests.spec.ts
+++ tests/sanity-tests/api-tests.spec.ts
@@ -8,5 +8,48 @@ test('Book a room using API', async ({request}) => {
         data: {"bookingdates":{"checkin":"2025-05-08","checkout":"2025-05-11"},"depositpaid":false,"firstname":"api","lastname":"booking","roomid":1,"email":"apiemail@email.com","phone":"01234567890"}
     })
     expect(response.status()).toBe(201);
-
+})
+test('Negative API test - Use incorrect date', async ({request}) => {
+    const response = await request.post('https://automationintesting.online/booking/', {
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        data: {"bookingdates":{"checkin":"2025-05-90","checkout":"2025-05-90"},"depositpaid":false,"firstname":"fail","lastname":"fail","roomid":1,"email":"apiemail@email.com","phone":"01234567890"}
+    })
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+})
+test('Negative API test - Use empty names', async ({request}) => {
+    const response = await request.post('https://automationintesting.online/booking/', {
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        data: {"bookingdates":{"checkin":"2025-07-01","checkout":"2025-07-03"},"depositpaid":false,"firstname":"","lastname":"","roomid":1,"email":"apiemail@email.com","phone":"01234567890"}
+    })
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+})
+test('Negative API test - Use invalid room number', async ({request}) => {
+    const response = await request.post('https://automationintesting.online/booking/', {
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        data: {"bookingdates":{"checkin":"2025-07-01","checkout":"2025-07-03"},"depositpaid":false,"firstname":"fail","lastname":"fail","roomid":11231,"email":"apiemail@email.com","phone":"01234567890"}
+    })
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+})
+test('Negative API test - Use invalid phone number', async ({request}) => {
+    const response = await request.post('https://automationintesting.online/booking/', {
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        data: {"bookingdates":{"checkin":"2025-07-01","checkout":"2025-07-03"},"depositpaid":false,"firstname":"fail","lastname":"fail","roomid":1,"email":"apiemail@email.com","phone":"this is not a number"}
+    })
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
 })
