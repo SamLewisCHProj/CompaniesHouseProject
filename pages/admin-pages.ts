0e38a69b7946d426024f279c1965d323e41f0469
spaddz <77231276+spaddz@users.noreply.github.com>
2025-01-23 13:38:47 +0000

    update admin page and api test

--- pages/admin-pages.ts
+++ pages/admin-pages.ts
@@ -5,12 +5,20 @@ export class adminPage {
   usernameField: Locator;
   passwordField: Locator;
   submitUserInfoButton: Locator;
-    
+  roomOptionDropdown: Locator;
+  accessibleRoomDropdown: Locator;
+  roomNumber: Locator;
+  roomPrice: Locator;
+
   constructor(page: Page) {
     this.page = page;
     this.usernameField = page.getByTestId("username");
     this.passwordField = page.getByTestId("password");
-    this.submitUserInfoButton = page.getByTestId("submit")
+    this.submitUserInfoButton = page.getByTestId("submit");
+    this.roomOptionDropdown = page.locator("#type");
+    this.accessibleRoomDropdown = page.locator("#accessible");
+    this.roomNumber = page.getByTestId("roomName");
+    this.roomPrice = page.locator("#roomPrice");
   }
   async adminLogin(page: Page) {
     await page.goto("https://automationintesting.online/#/admin");
@@ -18,7 +26,13 @@ export class adminPage {
     await this.passwordField.fill("password");
     await this.submitUserInfoButton.click();
   }
-  async verifyBooking(page:Page,firstName:string, lastName:string, email:string, mobile:string){
+  async verifyBooking(
+    page: Page,
+    firstName: string,
+    lastName: string,
+    email: string,
+    mobile: string
+  ) {
     await page.goto("https://automationintesting.online/#/admin/messages");
     await page.getByText(`${firstName} ${lastName}`).last().click();
     await expect(page.getByText(`Email: ${email}`)).toBeVisible();
@@ -27,11 +41,87 @@ export class adminPage {
     ).toBeVisible();
     await expect(page.getByText(`Phone: ${mobile}`)).toBeVisible();
   }
-  async verifyEnquiry(page:Page,subject:string, description: string){
+  async verifyEnquiry(page: Page, subject: string, description: string) {
     await page.goto("https://automationintesting.online/#/admin/messages");
     await page.getByText(subject).click();
     await expect(page.getByText(description)).toBeVisible();
     await page.getByRole("button", { name: "Close" }).click();
   }
+  async roomType(type: "Single" | "Twin" | "Double" | "Family" | "Suite") {
+    let value: string;
+    switch (type) {
+      case "Single":
+        value = "Single";
+        break;
+      case "Twin":
+        value = "Twin";
+        break;
+      case "Double":
+        value = "Double";
+        break;
+      case "Family":
+        value = "Family";
+        break;
+      case "Suite":
+        value = "Suite";
+        break;
+      default:
+        throw new Error("No room type selected");
+    }
+    await this.roomOptionDropdown.selectOption(value);
+  }
+  async roomAccessible(type: "true" | "false") {
+    let value: string;
+    switch (type) {
+      case "true":
+        value = "true";
+        break;
+      case "false":
+        value = "false";
+        break;
+      default:
+        throw new Error("No room accessibility specified");
+    }
+    await this.accessibleRoomDropdown.selectOption(value);
+  }
+  async roomFeatures(
+    options: Array<"Wifi" | "TV" | "Radio" | "Refreshments" | "Safe" | "Views">
+  ) {
+    if (options.length == 0) {
+    }
+    const validOptions = [
+      "Wifi",
+      "TV",
+      "Radio",
+      "Refreshments",
+      "Safe",
+      "Views",
+    ];
+    for (const option of options) {
+      if (!validOptions.includes(option)) {
+        throw new Error(`Invalid option selected ${option}`);
+      }
+      await this.page.getByLabel(option).check();
+    }
+  }
+  async addRoom(
+    number: string,
+    type: "Single" | "Double" | "Twin",
+    accessibility: "true" | "false",
+    price: string,
+    options: Array<"Wifi" | "TV" | "Radio" | "Refreshments" | "Safe" | "Views">
+  ) {
+    const roomNumberTaken = await this.page
+      .locator(`\ text="${number}"`)
+      .count();
+    if (roomNumberTaken > 0) {
+      return;
+    }
+    await this.roomNumber.fill(number);
+    await this.roomType(type);
+    await this.roomAccessible(accessibility);
+    await this.roomPrice.fill(price);
+    await this.roomFeatures(options);
+    await this.page.getByRole("button", { name: "Create" }).click();
+  }
 }
-
--- tests/sanity-tests/api-tests.spec.ts
+++ tests/sanity-tests/api-tests.spec.ts
@@ -1,152 +1,145 @@
 import { test, expect, Page } from "@playwright/test";
+import { adminPage } from "../../pages/admin-pages";
 
-// test.before(async ({ request }) => {
-//   const response = await request.post(
-//     "https://automationintesting.online/room/",
-//     {
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       data: {
-//         roomName: "15",
-//         type: "Double",
-//         accessible: false,
-//         description: "New room for testing",
-//         image: "https://www.mwtestconsultancy.co.uk/img/room1.jpg",
-//         roomPrice: "300",
-//         features: ["WiFi", "TV", "Safe", "Views"],
-//       },
-//     }
-//   );
-//   expect(response.status()).toBe(201);
-// });
-test("Book a room using API", async ({ request }) => {
-  const response = await request.post(
-    "https://automationintesting.online/booking/",
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        bookingdates: { checkin: "2025-05-08", checkout: "2025-05-11" },
-        depositpaid: false,
-        firstname: "api",
-        lastname: "booking",
-        roomid: 1,
-        email: "apiemail@email.com",
-        phone: "01234567890",
-      },
-    }
-  );
-  expect(response.status()).toBe(201);
-});
-test("Negative API test - Use incorrect date", async ({ request }) => {
-  const response = await request.post(
-    "https://automationintesting.online/booking/",
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        bookingdates: { checkin: "2025-05-90", checkout: "2025-05-90" },
-        depositpaid: false,
-        firstname: "fail",
-        lastname: "fail",
-        roomid: 1,
-        email: "apiemail@email.com",
-        phone: "01234567890",
-      },
-    }
-  );
-  expect(response.status()).toBe(400);
-  const responseText = await response.text();
-  console.log(`Response body: ${responseText}`);
-});
-test("Negative API test - Use empty names", async ({ request }) => {
-  const response = await request.post(
-    "https://automationintesting.online/booking/",
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        bookingdates: { checkin: "2025-07-01", checkout: "2025-07-03" },
-        depositpaid: false,
-        firstname: "",
-        lastname: "",
-        roomid: 1,
-        email: "apiemail@email.com",
-        phone: "01234567890",
-      },
-    }
-  );
-  expect(response.status()).toBe(400);
-  const responseText = await response.text();
-  console.log(`Response body: ${responseText}`);
-});
-test("Negative API test - Use invalid room number", async ({ request }) => {
-  const response = await request.post(
-    "https://automationintesting.online/booking/",
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        bookingdates: { checkin: "2025-07-01", checkout: "2025-07-03" },
-        depositpaid: false,
-        firstname: "fail",
-        lastname: "fail",
-        roomid: 11231,
-        email: "apiemail@email.com",
-        phone: "01234567890",
-      },
-    }
-  );
-  expect(response.status()).toBe(400);
-  const responseText = await response.text();
-  console.log(`Response body: ${responseText}`);
-});
-test("Negative API test - Use invalid phone number", async ({ request }) => {
-  const response = await request.post(
-    "https://automationintesting.online/booking/",
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        bookingdates: { checkin: "2025-07-01", checkout: "2025-07-03" },
-        depositpaid: false,
-        firstname: "fail",
-        lastname: "fail",
-        roomid: 1,
-        email: "apiemail@email.com",
-        phone: "this is not a number",
-      },
-    }
-  );
-  expect(response.status()).toBe(400);
-  const responseText = await response.text();
-  console.log(`Response body: ${responseText}`);
-});
-test("Negative API test - Use invalid email", async ({ request }) => {
-  const response = await request.post(
-    "https://automationintesting.online/booking/",
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        bookingdates: { checkin: "2025-07-01", checkout: "2025-07-03" },
-        depositpaid: false,
-        firstname: "fail",
-        lastname: "fail",
-        roomid: 1,
-        email: "a",
-        phone: "01234567890",
-      },
-    }
-  );
-  expect(response.status()).toBe(400);
-  const responseText = await response.text();
-  console.log(`Response body: ${responseText}`);
+test.describe("Sanity Checks", () => {
+  test("Book a room using API", async ({ request, page }) => {
+    const admin = new adminPage(page);
+    await admin.adminLogin(page);
+    await admin.addRoom("15", "Double", "true", "200", [
+      "Views",
+      "Radio",
+      "Refreshments",
+      "TV",
+    ]);
+    const response = await request.post(
+      "https://automationintesting.online/booking/",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          bookingdates: { checkin: "2025-05-08", checkout: "2025-05-11" },
+          depositpaid: false,
+          firstname: "api",
+          lastname: "booking",
+          roomid: 15,
+          email: "apiemail@email.com",
+          phone: "01234567890",
+        },
+      }
+    );
+    expect(response.status()).toBe(201);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+  });
+  test("Negative API test - Use incorrect date", async ({ request }) => {
+    const response = await request.post(
+      "https://automationintesting.online/booking/",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          bookingdates: { checkin: "2025-05-90", checkout: "2025-05-90" },
+          depositpaid: false,
+          firstname: "fail",
+          lastname: "fail",
+          roomid: 15,
+          email: "apiemail@email.com",
+          phone: "01234567890",
+        },
+      }
+    );
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+  });
+  test("Negative API test - Use empty names", async ({ request }) => {
+    const response = await request.post(
+      "https://automationintesting.online/booking/",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          bookingdates: { checkin: "2025-07-01", checkout: "2025-07-03" },
+          depositpaid: false,
+          firstname: "",
+          lastname: "",
+          roomid: 15,
+          email: "apiemail@email.com",
+          phone: "01234567890",
+        },
+      }
+    );
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+  });
+  test("Negative API test - Use invalid room number", async ({ request }) => {
+    const response = await request.post(
+      "https://automationintesting.online/booking/",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          bookingdates: { checkin: "2025-07-01", checkout: "2025-07-03" },
+          depositpaid: false,
+          firstname: "fail",
+          lastname: "fail",
+          roomid: 11231,
+          email: "apiemail@email.com",
+          phone: "01234567890",
+        },
+      }
+    );
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+  });
+  test("Negative API test - Use invalid phone number", async ({ request }) => {
+    const response = await request.post(
+      "https://automationintesting.online/booking/",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          bookingdates: { checkin: "2025-07-01", checkout: "2025-07-03" },
+          depositpaid: false,
+          firstname: "fail",
+          lastname: "fail",
+          roomid: 15,
+          email: "apiemail@email.com",
+          phone: "this is not a number",
+        },
+      }
+    );
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+  });
+  test("Negative API test - Use invalid email", async ({ request }) => {
+    const response = await request.post(
+      "https://automationintesting.online/booking/",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          bookingdates: { checkin: "2025-07-01", checkout: "2025-07-03" },
+          depositpaid: false,
+          firstname: "fail",
+          lastname: "fail",
+          roomid: 15,
+          email: "a",
+          phone: "01234567890",
+        },
+      }
+    );
+    expect(response.status()).toBe(400);
+    const responseText = await response.text();
+    console.log(`Response body: ${responseText}`);
+  });
 });
