35acf30da588687bbecf012e5460c3b3ae27a18f
spaddz <77231276+spaddz@users.noreply.github.com>
2025-01-24 14:58:38 +0000

    add basic validation test

--- tests/sanity-tests/admin-page-validation.spec.ts
+++ tests/sanity-tests/admin-page-validation.spec.ts
@@ -42,4 +42,48 @@ test.describe("Admin validation tests", () => {
       page.getByText("must be greater than or equal to 1")
     ).toBeVisible();
   });
+  test("Remove info and check field validation on bnb information", async ({
+    page,
+  }) => {
+    await page.getByRole("link", { name: "Branding" }).click();
+    await page.getByPlaceholder("Enter B&B name").click();
+    await page.getByPlaceholder("Enter B&B name").fill("");
+    await page.getByPlaceholder("Enter image url").click();
+    await page.getByPlaceholder("Enter image url").fill("");
+    await page.getByText("Welcome to Shady Meadows, a").click();
+    await page.getByText("Welcome to Shady Meadows, a").clear;
+    await page.getByPlaceholder("Enter Contact Name").click();
+    await page.getByPlaceholder("Enter Contact Name").clear();
+    await page.getByPlaceholder("Enter Address").click();
+    await page.getByPlaceholder("Enter Address").clear();
+    await page.getByPlaceholder("Enter Phone Number").click();
+    await page.getByPlaceholder("Enter Phone Number").clear();
+    await page.getByPlaceholder("Enter Email Address").click();
+    await page.getByPlaceholder("Enter Email Address").clear();
+    await page.getByRole("button", { name: "Submit" }).click();
+    await expect(page.getByText("Url should not be blank")).toBeVisible();
+    await expect(
+      page.getByText("Description should not be blank")
+    ).toBeVisible();
+    await expect(page.getByText("Name should not be blank")).toBeVisible();
+    await expect(
+      page.getByText("size must be between 3 and 500")
+    ).toBeVisible();
+    await expect(
+      page.getByText("size must be between 10 and 200")
+    ).toBeVisible();
+    await expect(
+      page.getByText("size must be between 3 and 100")
+    ).toBeVisible();
+    await expect(page.getByText("Email should not be blank")).toBeVisible();
+    await expect(page.getByText("Address should not be blank")).toBeVisible();
+    await expect(
+      page.getByText("Contact Name should not be blank")
+    ).toBeVisible();
+    await expect(
+      page.getByText("must be greater than or equal to 11")
+    ).toBeVisible();
+    await expect(page.getByText("Phone should not be blank")).toBeVisible();
+    await expect(page.getByText("size must be between 3 and 40")).toBeVisible();
+  });
 });
